364a58859a27ad9be1ad79ef4f3cd6516e7d4d77
Yaël Dillies <yael.dillies@gmail.com>
2026-09-11 12:24:53 +0200

    fix

--- Toric/GroupScheme/Diagonalizable.lean
+++ Toric/GroupScheme/Diagonalizable.lean
@@ -194,8 +194,8 @@ lemma commHopfAlgCatEquivCogrpCommAlgCat_functor_map_ofHom_mul
   ext1
   apply Quiver.Hom.unop_inj
   ext1
-  convert_to! (f * g).ofConv =
-    (Algebra.TensorProduct.lift (AlgHomClass.toAlgHom f.ofConv) (AlgHomClass.toAlgHom g.ofConv)
+  convert_to! (f * g).ofConv.toAlgHom =
+    (Algebra.TensorProduct.lift f.ofConv g.ofConv
       (fun _ _ ↦ .all _ _)).comp (Bialgebra.comulAlgHom _ _)
   dsimp [BialgHom.convMul_def]
   rw [← AlgHom.comp_assoc]
